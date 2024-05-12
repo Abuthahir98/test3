66a0bdc4661ecc0d218b0351668ff1444b74d54a
Abuthahir98 <127249703+Abuthahir98@users.noreply.github.com>
2024-05-12 15:41:18 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/add', methods=['POST'])
+def add():
+    num1 = float(request.form['num1'])
+    num2 = float(request.form['num2'])
+    result = num1 + num2
+    return str(result)
+
+@app.route('/subtract', methods=['POST'])
+def subtract():
+    num1 = float(request.form['num1'])
+    num2 = float(request.form['num2'])
+    result = num1 - num2
+    return str(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
