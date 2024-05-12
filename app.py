8dd996128debd5a7fba0ac7efef3eff3ab500900
Abuthahir98 <127249703+Abuthahir98@users.noreply.github.com>
2024-05-12 15:48:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,23 +2,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-#@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
-#@app.route('/add', methods=['POST'])
-def add():
-    num1 = float(request.form['num1'])
-    num2 = float(request.form['num2'])
-    result = num1 + num2
-    return str(result)
-
-#@app.route('/subtract', methods=['POST'])
-def subtract():
-    num1 = float(request.form['num1'])
-    num2 = float(request.form['num2'])
-    result = num1 - num2
-    return str(result)
+    result = None
+    if request.method == 'POST':
+        num1 = float(request.form['num1'])
+        num2 = float(request.form['num2'])
+        operation = request.form['operation']
+        if operation == 'add':
+            result = num1 + num2
+        elif operation == 'subtract':
+            result = num1 - num2
+    return render_template('index.html', result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
